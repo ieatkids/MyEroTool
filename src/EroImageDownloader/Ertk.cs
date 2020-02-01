aeefbd264c748699b7b0c8daa8f4581f8b7945d3
ieatkids <ieatkids@outlook.com>
2020-02-01 11:09:15 +0800

    first commit

---
+++ src/EroImageDownloader/Ertk.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace MyEroTool.EroImageDownloader
+{
+    public class Ertk : EroSite
+    {
+        private const string
+            Scheme = "http",
+            Host = "ertk.net",
+            ArticleXpath = "//h1[@class=\"postTitle\"]//a",
+            ImageXpath = "//span[@class=\"img_frame\"]//a";
+
+        protected override List<EroArticle> GetArticles()
+        {
+            var nodes = new HtmlWeb().Load(new UriBuilder("http", Host).Uri)
+                .DocumentNode.SelectNodes(ArticleXpath);
+            return (
+                from t in nodes
+                let uri = new Uri(t.Attributes["href"].Value)
+                select new EroArticle
+                    {Site = "ertk", Title = t.InnerText, Uri = uri, Id = uri.Segments[^1].Split('.')[0]}
+            ).ToList();
+        }
+
+        protected override List<Uri> GetImageUris(EroArticle eroArticle)
+        {
+            var nodes = new HtmlWeb().Load(eroArticle.Uri).DocumentNode.SelectNodes(ImageXpath);
+            return nodes.Select(node => new UriBuilder
+                {Scheme = Scheme, Host = Host, Path = node.Attributes["href"].Value}.Uri).ToList();
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+﻿using MyEroTool.EroImageDownloader;
+
+
+namespace MyEroTool
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var site = new Ertk();
+            site.Run();
+        }
+    }
+}
